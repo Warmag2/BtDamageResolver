3c3c610caa0db1c3e4eda013f78f1d03a7094edb
Vantte Kilappa <vantte.kilappa@mangosolutions.fi>
2021-09-08 08:45:17 +0300

    fix: Buildsystem errors.

--- build_producenugets.bat
+++ build_producenugets.bat
@@ -1,5 +1,8 @@
 del BtDamageResolver\src\Api\bin\Release\Faemiyah.BtDamageResolver.Api.*.nupkg
 del BtDamageResolver\src\Common\bin\Release\Faemiyah.BtDamageResolver.Common.*.nupkg
+del CompressionLzma\src\CompressionLzma\bin\Release\SevenZip.Compression.LZMA.*.nupgk
 dotnet build BtDamageResolver\BtDamageResolver.sln -c Release
+dotnet build CompressionLzma\CompressionLzma.sln -c Release
 copy BtDamageResolver\src\Api\bin\Release\*.nupkg CustomNugets
 copy BtDamageResolver\src\Common\bin\Release\*.nupkg CustomNugets
+copy CompressionLzma\src\CompressionLzma\bin\Release\*.nupkg CustomNugets
--- build_producenugets.sh
+++ build_producenugets.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 rm BtDamageResolver/src/Api/bin/Release/Faemiyah.BtDamageResolver.Api.*.nupkg
 rm BtDamageResolver/src/Common/bin/Release/Faemiyah.BtDamageResolver.Common.*.nupkg
+rm CompressionLzma/src/CompressionLzma/bin/Release/SevenZip.Compression.LZMA.*.nupgk
 dotnet build BtDamageResolver/BtDamageResolver.sln -c Release
+dotnet build CompressionLzma/CompressionLzma.sln -c Release
 cp BtDamageResolver/src/Api/bin/Release/*.nupkg CustomNugets
 cp BtDamageResolver/src/Common/bin/Release/*.nupkg CustomNugets
+cp CompressionLzma/src/CompressionLzma/bin/Release/*.nupkg CustomNugets
