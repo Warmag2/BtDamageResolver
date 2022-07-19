6609ec648a8b13f6acc390c22ee4aab01216c86d
Vantte Kilappa <warma@iki.fi>
2022-07-19 14:32:33 +0300

    fix: Clarify build batch files a bit.

--- build_pushnugets.bat
+++ build_pushnugets.bat
@@ -1,5 +1,4 @@
 REM This batch file is only for the author's own use. You can safely ignore it.
-call build_rollversion.bat
-call build_producenugets.bat
 dotnet nuget push "BtDamageResolver\src\Api\bin\Release\Faemiyah.BtDamageResolver.Api.*.nupkg" --source "githubWarma" --api-key %BtDamageResolverNugetKey%
 dotnet nuget push "BtDamageResolver\src\Common\bin\Release\Faemiyah.BtDamageResolver.Common.*.nupkg" --source "githubWarma" --api-key %BtDamageResolverNugetKey%
+dotnet nuget push "CompressionLzma\src\CompressionLzma\bin\Release\SevenZip.Compression.LZMA.*.nupkg" --source "githubWarma" --api-key %BtDamageResolverNugetKey%
