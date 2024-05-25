b10bbd68ab2cb4920c07564630a9ed8daf0dc8a6
Vantte Kilappa <warma@iki.fi>
2024-05-25 14:53:28 +0300

    fix: pushnugets.bat

--- build_pushnugets.bat
+++ build_pushnugets.bat
@@ -1,4 +1,4 @@
 REM This batch file is only for the author's own use. You can safely ignore it.
-dotnet nuget push "BtDamageResolver\src\Api\bin\Release\Faemiyah.BtDamageResolver.Api.*.nupkg" --source "githubWarma" --api-key %BtDamageResolverNugetKey%
-dotnet nuget push "BtDamageResolver\src\Common\bin\Release\Faemiyah.BtDamageResolver.Common.*.nupkg" --source "githubWarma" --api-key %BtDamageResolverNugetKey%
-dotnet nuget push "CompressionLzma\src\CompressionLzma\bin\Release\SevenZip.Compression.LZMA.*.nupkg" --source "githubWarma" --api-key %BtDamageResolverNugetKey%
+dotnet nuget push "BtDamageResolver\src\Api\bin\Release\Faemiyah.BtDamageResolver.Api.*.nupkg" --skip-duplicate --source "githubWarma" --api-key %BtDamageResolverNugetKey%
+dotnet nuget push "BtDamageResolver\src\Common\bin\Release\Faemiyah.BtDamageResolver.Common.*.nupkg" --skip-duplicate --source "githubWarma" --api-key %BtDamageResolverNugetKey%
+dotnet nuget push "CompressionLzma\src\CompressionLzma\bin\Release\SevenZip.Compression.LZMA.*.nupkg" --skip-duplicate --source "githubWarma" --api-key %BtDamageResolverNugetKey%
