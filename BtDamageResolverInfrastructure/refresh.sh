a700073fa318a642202a4f9c2bb00b18793ac2f5
Vantte Kilappa <warma@iki.fi>
2025-04-03 19:00:43 +0300

    feat: Automate sdk creation.

--- BtDamageResolverInfrastructure/refresh.sh
+++ BtDamageResolverInfrastructure/refresh.sh
@@ -4,7 +4,8 @@ chmod -R o+rx dashboards/
 chmod -R o+rx datasources/
 docker build --tag resolvergrafana -f ./grafana/Dockerfile ./grafana/
 docker build --tag resolverredis -f ./redis/Dockerfile ./redis/
-docker build --tag resolversdk -f ../CustomNugets/Dockerfile ../CustomNugets/
+#docker build --tag resolversdk -f ../CustomNugets/Dockerfile ../CustomNugets/
+docker build --tag resolversdk -f ../sdk/Dockerfile ../
 docker build --tag resolver -f ../BtDamageResolver/src/Silo/Dockerfile ../BtDamageResolver/
 docker build --tag resolverdataimporter -f ../BtDamageResolver/tools/DataImporter/Dockerfile ../BtDamageResolver/
 docker build --tag resolverclient -f ../BtDamageResolverClient/src/BlazorServer/Dockerfile ../BtDamageResolverClient/
