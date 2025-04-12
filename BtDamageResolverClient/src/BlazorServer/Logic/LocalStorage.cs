e0d1fafc898eee320c387e8a1ef8b7133dbcd5da
Vantte Kilappa <warma@iki.fi>
2025-04-12 17:39:54 +0300

    fix: Try to get rid of a deprecated nuget.

--- BtDamageResolverClient/src/BlazorServer/Logic/LocalStorage.cs
+++ BtDamageResolverClient/src/BlazorServer/Logic/LocalStorage.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using Faemiyah.BtDamageResolver.Api.Entities;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
@@ -10,6 +11,7 @@ namespace Faemiyah.BtDamageResolver.Client.BlazorServer.Logic;
 /// </summary>
 public class LocalStorage
 {
+    private const string LocalStorageStoreName = "BtDamageResolverClient";
     private const string LocalStorageVariableUserCredentials = "BtDamageResolverUserCredentials";
     private readonly ILocalStorageService _localStorageService;
     private readonly ProtectedSessionStorage _protectedSessionStorage;
@@ -18,6 +20,7 @@ public class LocalStorage
     /// Initializes a new instance of the <see cref="LocalStorage"/> class.
     /// </summary>
     /// <param name="localStorageService">The local storage service.</param>
+    /// <param name="protectedSessionStorage">Protected session storage service.</param>
     public LocalStorage(ILocalStorageService localStorageService, ProtectedSessionStorage protectedSessionStorage)
     {
         _localStorageService = localStorageService;
@@ -29,6 +32,45 @@ public class LocalStorage
     /// </summary>
     /// <returns>A tuple with the success status of the fetch, and the credentials, if found.</returns>
     public async Task<(bool Success, Credentials Credentials)> GetUserCredentials()
+    {
+        var credentialsResult = await _protectedSessionStorage.GetAsync<Credentials>(LocalStorageStoreName, LocalStorageVariableUserCredentials);
+
+        if (credentialsResult.Success)
+        {
+            if (!string.IsNullOrWhiteSpace(credentialsResult.Value.Name))
+            {
+                return (true, credentialsResult.Value);
+            }
+        }
+
+        return (false, null);
+    }
+
+    /// <summary>
+    /// Sets user credentials.
+    /// </summary>
+    /// <param name="credentials">The credentials to set to.</param>
+    /// <returns>A task which finishes when the credentials are set.</returns>
+    public async Task SetUserCredentials(Credentials credentials)
+    {
+        await _protectedSessionStorage.SetAsync(LocalStorageStoreName, LocalStorageVariableUserCredentials, credentials);
+    }
+
+    /// <summary>
+    /// Removes user credentials.
+    /// </summary>
+    /// <returns>A task which finishes when the credentials are removed.</returns>
+    public async Task RemoveUserCredentials()
+    {
+        await _protectedSessionStorage.DeleteAsync(LocalStorageVariableUserCredentials);
+    }
+
+    /// <summary>
+    /// Gets user credentials from local storage.
+    /// </summary>
+    /// <returns>A tuple with the success status of the fetch, and the credentials, if found.</returns>
+    [Obsolete("Deprecated nuget")]
+    public async Task<(bool Success, Credentials Credentials)> GetUserCredentialsOld()
     {
         if (await _localStorageService.ContainKeyAsync(LocalStorageVariableUserCredentials))
         {
@@ -48,7 +90,8 @@ public class LocalStorage
     /// </summary>
     /// <param name="credentials">The credentials to set to.</param>
     /// <returns>A task which finishes when the credentials are set.</returns>
-    public async Task SetUserCredentials(Credentials credentials)
+    [Obsolete("Deprecated nuget")]
+    public async Task SetUserCredentialsOld(Credentials credentials)
     {
         await _localStorageService.SetItemAsync(LocalStorageVariableUserCredentials, credentials);
     }
@@ -57,7 +100,8 @@ public class LocalStorage
     /// Removes user credentials.
     /// </summary>
     /// <returns>A task which finishes when the credentials are removed.</returns>
-    public async Task RemoveUserCredentials()
+    [Obsolete("Deprecated nuget")]
+    public async Task RemoveUserCredentialsOld()
     {
         await _localStorageService.RemoveItemAsync(LocalStorageVariableUserCredentials);
     }
