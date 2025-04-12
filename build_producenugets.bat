bbbfd356913dde0b3403648cd7a3dbda455e51b1
Vantte Kilappa <warma@iki.fi>
2025-04-12 20:38:41 +0300

    fix: CustomNugets are no longer included in the repository, so the batch file must try to create the directory.

--- build_producenugets.bat
+++ build_producenugets.bat
@@ -1,3 +1,4 @@
+mkdir CustomNugets
 del BtDamageResolver\src\Api\bin\Release\Faemiyah.BtDamageResolver.Api.*.nupkg
 del BtDamageResolver\src\Common\bin\Release\Faemiyah.BtDamageResolver.Common.*.nupkg
 del CompressionLzma\src\CompressionLzma\bin\Release\SevenZip.Compression.LZMA.*.nupkg
