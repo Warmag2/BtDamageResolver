181b2bb896c49feade48efb5623c0e6047d3a2e2
Vantte Kilappa <warma@iki.fi>
2026-05-22 23:38:58 +0300

    fix: Missing usings.

--- BtDamageResolverClient/src/BlazorServer/Startup.cs
+++ BtDamageResolverClient/src/BlazorServer/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Text.Json;
 using Faemiyah.BtDamageResolver.Api.ClientInterface.Compression;
 using Faemiyah.BtDamageResolver.Api.ClientInterface.Repositories;
